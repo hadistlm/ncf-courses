30f0768f063cae0c91225584dca6ff10ce28b537
Islaa <hadist.lm@gmail.com>
2024-11-21 02:19:06 +0700

    Adding: convert course data using orjson converter

--- helpers/convertCourse.py
+++ helpers/convertCourse.py
@@ -1,12 +1,14 @@
-import json
+import os
+import orjson as json
 
 # Load the original JSON data
-with open('../data/raw_course.json', 'r') as file:
-    data = json.load(file)
+file_path = os.path.join(os.path.dirname(__file__), '../data/raw_course.json')
+with open(file_path, 'rb') as f:
+    data = json.loads(f.read())
 
 # Filter the data for courses where the Site is "Coursera"
 coursera_courses = [course for course in data if course.get('Site') == 'Coursera']
 
 # Save the filtered data to a new JSON file
-with open('../data/course.json', 'w') as outfile:
-    json.dump(coursera_courses, outfile, indent=4)
+with open(os.path.join(os.path.dirname(__file__), '../data/course.json'), 'wb') as outfile:
+    outfile.write(json.dumps(coursera_courses, option=json.OPT_INDENT_2))
